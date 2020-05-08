20054bc35979fb11a372f0dd219d37b19bf2740e
Akila <akilarajesh20@gmail.com>
2020-05-08 01:56:30 +0100

    Implemented leak demo

---
+++ app/src/main/java/com/zestworks/androidsamplekit/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.zestworks.androidsamplekit
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_navigate_leak.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_leakFragment)
+        }
+    }
+}
---
+++ concept-memoryleak/src/main/java/com/zestworks/concept_memoryleak/LeakFragment.kt
@@ -0,0 +1,29 @@
+package com.zestworks.concept_memoryleak
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_leak.*
+import java.util.concurrent.TimeUnit
+
+
+class LeakFragment : Fragment(R.layout.fragment_leak) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_action_leak.setOnClickListener {
+            LeakyThread(activity!!).start()
+            Toast.makeText(context, "Rotate for memory leak", Toast.LENGTH_SHORT).show()
+        }
+
+        webView.loadUrl("https://raw.githubusercontent.com/prithivraj/DistanceFilter/master/src/main/java/Main.kt")
+    }
+}
+
+class LeakyThread(val activity: Activity) : Thread() {
+    override fun run() {
+        super.run()
+        sleep(TimeUnit.MINUTES.toMillis(3))
+    }
+}
